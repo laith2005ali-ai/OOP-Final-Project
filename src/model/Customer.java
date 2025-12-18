86d8246a6a993c216fa7655735b9b58916dbfe37
Aras777 <Aras777@LAPTOP-89E86HT1>
2025-12-18 18:54:48 +0300

    add Customer class with core fields and constructor

---
+++ src/model/Customer.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Customer {
+    // Fields
+    private String customerId;
+    private String name;
+    private String phone;
+    
+    // Constructor
+    public Customer(String customerId, String name, String phone) {
+        this.customerId = customerId;
+        this.name = name;
+        this.phone = phone;
+    }
+}
